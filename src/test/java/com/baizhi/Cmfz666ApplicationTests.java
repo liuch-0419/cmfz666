48399ca402bb21677315735cdb27d865898585b4
liuch <309573024@qq.com>
2019-08-11 11:16:01 +0800

    小黑第一次提交

--- src/test/java/com/baizhi/Cmfz666ApplicationTests.java
+++ src/test/java/com/baizhi/Cmfz666ApplicationTests.java
@@ -16,6 +16,8 @@ public class Cmfz666ApplicationTests {
         System.out.println("===2===");
 
         System.out.println("===3===");
+
+        System.out.println("===我是小黑===");
         
     }
 
